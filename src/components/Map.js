9cd733e33639188aaa684af71ca7523ce8b49eb9
Andrés Kaminker <andykamin3@gmail.com>
2022-09-12 08:24:08 -0300

    added button for wallet connection

--- src/components/Map.js
+++ src/components/Map.js
@@ -19,6 +19,9 @@ export default function SimpleMap(props){
 
   useEffect(() => {
     if (place!==null && place!==undefined && map !== null) {
+      if (place.geometry===undefined) {
+        return;
+      }
       const lat = place.geometry.location.lat();
       const lng = place.geometry.location.lng();
       console.log(lat, lng);
@@ -35,13 +38,15 @@ export default function SimpleMap(props){
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
-        onClick={(e) => console.log(e.event.target)}
+
         yesIWantToUseGoogleMapApiInternals
         onGoogleApiLoaded={({ map, maps }) => {
           setMap(map);
         }}
       >
-        <Avatar  alt="Remy Sharp"  src="https://mui.com/static/images/avatar/1.jpg"  lat={-34.603722} lng={-58.3819}/>
+        <Avatar  alt="Remy Sharp"  src="https://mui.com/static/images/avatar/1.jpg"  lat={-34.603722} lng={-58.3819}
+          onClick={() => console.log("You clicked me!")}
+        />
 
       </GoogleMapReact>
     </div>
